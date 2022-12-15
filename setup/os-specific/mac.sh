860ed7b2c428ad26d746155e26c1f28aa3524767
Alexander Ding <alexander.jf.ding@gmail.com>
2022-12-15 04:41:14 -0500

    chore: remove mac-specific set ups in favor of syncing settings

--- setup/os-specific/mac.sh
+++ setup/os-specific/mac.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-printf "\nExecuting MacOS-specific setups\n"
-echo "- always show hidden files"
-defaults write com.apple.finder AppleShowAllFiles TRUE
+# printf "\nExecuting macOS-specific setups\n"
+# echo "- setting sensible defaults"
